8bdf97495700f5611ba871c3f3bb784d9fe4ea58
carabrook <cbrook04@gmail.com>
2023-02-02 10:26:27 -0600

    updates

--- CourseMaterial/Week5/code/simplepop.R
+++ CourseMaterial/Week5/code/simplepop.R
@@ -361,30 +361,347 @@ head(Ncontinuous)
 subset(Ncontinuous,time==2)
 
 ## Now we can also plot the full time-series:  
+Ncontinuous <- data.frame(Ncontinuous)
 Ncontinuous$type="continuous"
 
-png(paste0(homewd, "figs/discrete_continuous.png"), width = 60, height = 45)
+Ndiscrete <- cbind.data.frame(time=1:100, N=N, type="discrete")
 
-plot(Ncontinuous[,"time"],               # Time on the x axis
-     Ncontinuous[,"N"],                  # Number people on the y axis
-     xlab = "Time in years",     # Label the x axis
-     ylab = "Pop size",         # Label the y axis
-     main = " ",                # Plot title
-     xlim = c(0,100),           # 
-     type = "l",                # Use a line plot
-     bty = "n")                 # Remove the box around the plot
-points(1:100,N, pch=19,col="red",cex=0.2)  #compare with our discrete time
-legend("topleft",legend=c("continuous","discrete"), lty=c(1,3), col=c("black", "red"), bty="n") #add legend
+Ncontinuous <- rbind(Ncontinuous, Ndiscrete)
 
-dev.off()
-# Note that we overlaid our discrete time model predictions (the variable N) for comparison.
-## This is close, but not an exact match to our discrete time version, which makes sense, 
-## since these are different processes.  
+colz=c('continuous'= "black", 'discrete'="red")
+p6 <- ggplot(data=Ncontinuous)+
+      geom_point(data=subset(Ncontinuous, type=="discrete"), aes(x=time, y=N, color=type), size=2) +
+      geom_line(data=subset(Ncontinuous, type!="discrete"), aes(x=time, y=N, color=type), size=1) +
+      theme_bw() + 
+      scale_color_manual(values=colz) +
+      theme(panel.grid = element_blank(), 
+        legend.position = c(.2,.8),
+        legend.title = element_blank(),
+        axis.title = element_text(size=18),
+        legend.text = element_text(size=12),
+        axis.text = element_text(size=16))
+
+ggsave(file = paste0(homewd, "figs/discrete_continuous.png"),
+       units="mm",  
+       width=60, 
+       height=45, 
+       scale=2.5, 
+       dpi=300)
 
 
-## TO DO: 
-#### 1) See what happens to a population where r<0
-#### 2) Change the time resolution (set by "time.out"") 
-####   and see if/where the results change
 
 
+################## logistic growth
+
+library(deSolve)
+library(reshape2)
+
+
+logistic_growth <- function(time, state, params){
+    with(as.list(c(state, params)), {
+      dXdt <- r * X  * (1 - X / K)
+      return(list(c(dXdt)))
+    })
+}
+
+  pars <- c(r = 0.25, K = 5)
+  xini <- c(X = 0.5)
+  times <- seq(0, 100, by = 0.05)
+  out <- as.data.frame(ode(xini, times, logistic_growth, pars))
+  out$K = 5
+  out$label="K=5"
+  
+  pars["K"] <- 10
+  out2 <- as.data.frame(ode(xini, times, logistic_growth, pars))
+  out2$K = 10
+  out2$label="K=10"
+  
+  out<- rbind(out, out2)
+  head(out)
+  m <- melt(out, id.vars = c("time", "K", "label"))
+  m$K<- as.character(m$K)
+  head(m)
+  
+  pl_logistic <- ggplot(m) + 
+    geom_line(aes(x = time, y = value, colour = K), size = 2, alpha = 0.7) + 
+    scale_x_continuous("Time t") + 
+    scale_y_continuous("X(t)", limits = c(0, NA)) +
+    geom_hline(yintercept = 5, linetype = 2, colour = "black") + 
+    geom_hline(yintercept = 10, linetype = 2, colour = "black") + 
+    theme_bw() + 
+    theme(axis.text=element_text(size=14, face="bold"), 
+          axis.title=element_text(size=14, face="bold"),
+          legend.position = c(.8,.2)) 
+    
+  show(pl_logistic)
+  
+
+
+
+  ################ 
+  
+  #estimating parameters from data  
+
+  head(world_pop)
+  
+
+comp.log.growth<- function(par, xini, times, data, K){
+  
+  #simulate logistic growth with the guess parameters
+  pars = c(par, K)
+  
+  out <- as.data.frame(ode(xini, times, logistic_growth, pars))
+  
+  #and compare with data
+  out$data <- data$pop
+  out$sq_diff <- ((out$data-out$X)^2)
+  
+  sm.sq = sum(out$sq_diff)
+  return(sm.sq)
+  
+}  
+fit.diff.subs <- function(data, pars, times, min.fit, max.fit){
+  
+  
+  dat = subset(data, year >=min.fit & year<=max.fit)
+  time.comp = dat$year
+  xini1=dat$pop[dat$year==min(dat$year)]
+  names(xini1) <- "X"
+  
+  fit <- optim(par=pars["r"], fn=comp.log.growth, xini=xini1, times=time.comp, data=dat, K=pars["K"],
+        method = "Nelder-Mead")
+  
+  #and return dataset of pars and fit range
+  fit.dat <- cbind.data.frame(r=fit$par["r"], K=pars["K"], min_fit=min.fit, max_fit =max.fit)
+
+  return(fit.dat)  
+}
+  
+  
+  
+#fit to 3 different subsets of years 
+#(1960:1970)
+#(1960:1990)
+#(1960:2018)
+
+
+join.all.fit <- function(test.dat, pars, year1, year2, year3){
+  
+  fit1 <- fit.diff.subs(data=test.dat, pars = pars, times=test.dat$year, min.fit =  min(test.dat$year), max.fit = year1)
+  fit2 <- fit.diff.subs(data=test.dat, pars = pars, times=test.dat$year, min.fit =  min(test.dat$year), max.fit = year2)
+  fit3 <- fit.diff.subs(data=test.dat, pars = pars, times=test.dat$year, min.fit =  min(test.dat$year), max.fit = year3)
+  
+  all.fit <- rbind(fit1, fit2, fit3)
+  
+  return(all.fit)
+}
+
+fit.pars = join.all.fit(test.dat=subset(world_pop, country=="USA"),
+                        pars = list(r=0.03, K=320000000*2),
+                        year1=1970,
+                        year2=1990,
+                        year3=2018)
+
+
+#and sim and plot
+sim.plot <- function(fit.pars, test.dat, plotK){
+  
+  if(nrow(fit.pars)==3){
+    
+    
+    
+    xini <- c(X = min(test.dat$pop))
+    times <- test.dat$year
+    out1 <- as.data.frame(ode(xini, times, logistic_growth, fit.pars[1,1:2]))
+    out2 <- as.data.frame(ode(xini, times, logistic_growth, fit.pars[2,1:2]))
+    out3 <- as.data.frame(ode(xini, times, logistic_growth, fit.pars[3,1:2]))
+    
+    out1$fit <- paste0("1960:", fit.pars[1,4], "; r=", round(fit.pars[1,1],3))
+    out2$fit <- paste0("1960:", fit.pars[2,4], "; r=", round(fit.pars[2,1],3))
+    out3$fit <- paste0("1960:", fit.pars[3,4], "; r=", round(fit.pars[2,1],3))
+    
+    
+    fit.dat <- rbind(out1,out2,out3)
+    
+  }else{
+    xini <- c(X = min(test.dat$pop))
+    times <- test.dat$year
+    fit.dat <- as.data.frame(ode(xini, times, logistic_growth, fit.pars[1,1:2]))
+    fit.dat$fit <- paste0("1960:", fit.pars[1,4], "; r=", round(fit.pars[1,1],3))
+    
+  }
+  
+  p <- ggplot(data = test.dat) + 
+    geom_point(aes(x = year, y = pop), size=3) + 
+    #geom_line(aes(x = year, y = pop), size=1)  + 
+    geom_line(data=fit.dat, aes(x = time, y = X,color=fit), size=1)  + 
+    theme_bw() + 
+    scale_y_continuous(labels = scales::comma, limits = range(test.dat$pop))+
+    scale_color_discrete("date range for fitting") +
+    theme(axis.text=element_text(size=14, face="bold"), 
+          axis.title=element_text(size=14, face="bold"),
+          legend.position = c(.25,.8),
+          legend.text = element_text(size=12),
+          legend.title = element_text(size=12)) +
+    ylab(paste0(unique(test.dat$country), " population"))
+  
+  if(plotK==T){
+    p <- ggplot(data = test.dat) + 
+      geom_point(aes(x = year, y = pop), size=3) + 
+      #geom_line(aes(x = year, y = pop), size=1)  + 
+      geom_line(data=fit.dat, aes(x = time, y = X,color=fit), size=1)  + 
+      geom_hline(yintercept = unique(fit.pars$K), linetype=2) +
+      theme_bw() + 
+      scale_y_continuous(labels = scales::comma, limits = range(test.dat$pop))+
+      scale_color_discrete("date range for fitting") +
+      theme(axis.text=element_text(size=14, face="bold"), 
+            axis.title=element_text(size=14, face="bold"),
+            legend.position = c(.25,.8),
+            legend.text = element_text(size=12),
+            legend.title = element_text(size=12)) +
+      ylab(paste0(unique(test.dat$country), " population"))
+    
+  }
+  
+  return(p)
+  
+}
+
+sim.plot(fit.pars = fit.pars, plotK = F,
+         test.dat=subset(world_pop, country=="USA"))
+
+ggsave(file = paste0(homewd, "figs/exponential_fit_USA.png"),
+       units="mm",  
+       width=60, 
+       height=45, 
+       scale=2.5, 
+       dpi=300)
+
+
+fit.pars2 = join.all.fit(test.dat=subset(world_pop, country=="Madagascar"),
+                        pars = list(r=0.03, K=320000000*2),
+                        year1=1970,
+                        year2=1990,
+                        year3=2018)
+
+sim.plot(fit.pars = fit.pars2, plotK = F,
+         test.dat=subset(world_pop, country=="Madagascar"))
+
+ggsave(file = paste0(homewd, "figs/exponential_fit_Madagascar.png"),
+       units="mm",  
+       width=60, 
+       height=45, 
+       scale=2.5, 
+       dpi=300)
+
+#and with K
+
+#fit to 3 different subsets of years 
+#(1960:1970)
+#(1960:1990)
+#(1960:2018)
+
+
+
+fit.pars = join.all.fit(test.dat=subset(world_pop, country=="USA"),
+                        pars = list(r=0.03, K=320000000),
+                        year1=1970,
+                        year2=1990,
+                        year3=2018)
+
+
+#and sim and plot
+
+
+sim.plot(fit.pars = subset(fit.pars, max_fit==1990),plotK = T,
+         test.dat=subset(world_pop, country=="USA"))
+
+ggsave(file = paste0(homewd, "figs/logistic_fit_USA.png"),
+       units="mm",  
+       width=60, 
+       height=45, 
+       scale=2.5, 
+       dpi=300)
+  
+
+
+
+fit.pars2 = join.all.fit(test.dat=subset(world_pop, country=="Madagascar"),
+                         pars = list(r=0.03, K=28000000),
+                         year1=1970,
+                         year2=1990,
+                         year3=2018)
+
+
+
+sim.plot(fit.pars = subset(fit.pars2, max_fit==1990),plotK = T,
+         test.dat=subset(world_pop, country=="Madagascar"))
+
+ggsave(file = paste0(homewd, "figs/logistic_fit_Madagascar.png"),
+       units="mm",  
+       width=60, 
+       height=45, 
+       scale=2.5, 
+       dpi=300)    
+
+
+
+###############
+#############
+# carrying capacity
+Xs <- seq(0, 1.15 * 5, length.out = 100)
+growth_rate_logistic <- data.frame(X = Xs, dXdt = 0.25 * Xs * (1 - Xs / 5))
+
+eq_logistic <- ggplot(growth_rate_logistic, 
+                      aes(x = X, y = dXdt)) + 
+  geom_line(size = 2, alpha = 0.5, colour = "black") +
+  scale_x_continuous("N(t)") + 
+  scale_y_continuous("dN(t)/dt") +
+  geom_point(x = 0, y = 0, shape = 1, size = 4) +
+  geom_point(x = 5, y = 0, shape = 1, size = 4) +
+  geom_hline(yintercept = 0) + 
+  geom_vline(xintercept = 0) + 
+  theme_bw() + 
+  theme(axis.text=element_text(size=14, face="bold"), axis.title=element_text(size=14, face="bold")) +
+  theme(legend.position = "none")
+show(eq_logistic)
+
+
+ggsave(file = paste0(homewd, "figs/dNdt_logistic.png"),
+       units="mm",  
+       width=60, 
+       height=45, 
+       scale=2.5, 
+       dpi=300)    
+
+
+
+###########
+###########
+#halley
+
+halley <- read.csv(paste0(homewd,"halley.csv"), header = T, stringsAsFactors = F)
+pl_halley <- ggplot(data = halley, aes(x = Age, y = Number)) + geom_line() + 
+  theme_bw() + 
+  theme(axis.text = element_text(size = 14, face = "bold"), 
+        axis.title = element_text(size=14, face="bold")) +
+  theme(legend.position = "none")
+show(pl_halley)
+
+
+#pyramid
+
+pl_halley_bar <- ggplot(data = halley, aes(x = Age, y = Number)) + geom_bar(stat = "identity") + 
+  theme_bw() + 
+  theme(axis.text = element_text(size = 14, face = "bold"), 
+        axis.title = element_text(size=14, face="bold")) +
+  theme(legend.position = "none") + 
+  coord_flip()
+show(pl_halley_bar)
+
+ggsave(file = paste0(homewd, "figs/halley_pyramid.png"),
+       units="mm",  
+       width=60, 
+       height=45, 
+       scale=2.5, 
+       dpi=300)   
